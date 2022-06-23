2fdc52c1bd7c6633bfca27e32e56f83594d17619
pooja <poojadevtech3@gmail.com>
2022-06-23 13:33:04 +0530

    updated directory structure

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Button } from "./button";
-import styles from "./form.module.css";
+import { Button } from "../Button/Button";
+import styles from "../../styles/form.module.css";
 
 type IFormProps = {
   "on-submit": (payload: { title: string; description: string; price: string }) => void;
@@ -17,13 +17,11 @@ export const Form: React.FC<IFormProps> = (props) => {
 
     if (!titleRef.current?.value) {
       alert("Your product needs a title");
-
       return;
     }
 
     if (!descriptionRef.current?.value || !priceRef.current?.value) {
       alert("Your product needs some content");
-
       return;
     }
 
@@ -39,30 +37,25 @@ export const Form: React.FC<IFormProps> = (props) => {
   return (
     <form className={styles.form} onSubmit={(event) => handleSubmit(event)} ref={formRef}>
       <span className={styles.label}>Product title: *</span>
-
       <input
         ref={titleRef}
         placeholder="Title..."
         defaultValue=""
         className={styles.input}
       />
-
       <span className={styles.label}>Product details: *</span>
-
       <input
         ref={priceRef}
         placeholder="Price..."
         defaultValue=""
         className={styles.input}
       />
-
       <textarea
         ref={descriptionRef}
         placeholder="Start typing product description here..."
         defaultValue=""
         className={styles.textarea}
       />
-
       <Button>Add a product</Button>
     </form>
   );
